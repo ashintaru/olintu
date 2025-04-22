953e0056340af89348190e37349aa1498c2cf840
CHRISTIAN ROASA <109757954+ashintaru@users.noreply.github.com>
2025-04-22 11:54:26 +0800

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,6 +8,7 @@
             <h1 class="text-2xl font-semibold">OLINTU - About</h1>
         </div>
     </nav>
+    sample
     <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">🌟 Introducing OLINTU: Your Gateway to Smart Learning! 🌐🧠</h1>
